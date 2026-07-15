e9a034c7f753728b7ddfc412970a5931965a534a
Pedro Jr Solatorio <solatoriop@gmail.com>
2026-07-15 18:27:09 +0800

    Added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import { Route, Routes, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-
-function HomePage() {
-  return <h1>Home Page</h1>;
-}
-
-function AboutPage() {
-  return <h1>About Page</h1>;
-}
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <nav style={{ display: "flex", gap: "1rem", marginBottom: "1rem" }}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
+    <div className="app">
+      <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/checkout" element={<Checkout />} />
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
-      <div>Footer</div>
     </div>
   );
 }
